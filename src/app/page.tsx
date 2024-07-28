05e8b4745d670dea61e6ea5b58a109cd1e604ae9
MRCDEV <126817362+marcossdev04@users.noreply.github.com>
2024-07-28 18:44:08 -0300

    initial-commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Container } from '@/components/Container'
+import { Hero } from '@/components/Hero'
+export default function Home() {
+  return (
+    <Container>
+      <Hero />
+    </Container>
+  )
+}
---
+++ src/app/templates/page.tsx
@@ -0,0 +1,35 @@
+import { Plus, SquarePen, Trash2 } from 'lucide-react'
+
+export default function Template() {
+  return (
+    <div className="mx-auto flex max-w-[600px] flex-col rounded-xl border bg-white bg-opacity-20 shadow-2xl ">
+      <div className="flex items-center justify-between border-b border-gray-600 p-3">
+        <div className="text-2xl font-medium">Modelos</div>
+        <div>
+          <input
+            className="border-primary pr-4.5 focus:border-primary border bg-white py-2 pl-2 text-black focus-visible:outline-none"
+            name="fullName"
+            id="fullName"
+            placeholder="Pesquisar"
+          />
+        </div>
+        <button className="rounded-full bg-green-500 p-1 hover:bg-opacity-80">
+          <Plus size={32} />
+        </button>
+      </div>
+      <div className="flex flex-col">
+        <div className="flex justify-between p-3">
+          <div className="text-xl font-medium">Hepatite</div>
+          <div className="flex gap-2">
+            <button>
+              <SquarePen size={27} className="hover:text-green-500" />
+            </button>
+            <button>
+              <Trash2 size={27} className="hover:text-red-600" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}

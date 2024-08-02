be10417e4563e9f70a241bdd8a630a10bfbc17ad
MRCDEV <126817362+marcossdev04@users.noreply.github.com>
2024-08-02 16:56:24 -0300

    advances

--- src/app/design/page.tsx
+++ src/app/design/page.tsx
@@ -1,29 +1,88 @@
 'use client'
-import { Printer } from 'lucide-react'
+import { Plus, Printer } from 'lucide-react'
 import { useState } from 'react'
 
 export default function Design() {
+  const [title, setTitle] = useState<string>('')
+
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value)
+  }
+  console.log(title)
+
   const [section, setSection] = useState<
     {
       title: string | null
-      description1: [string] | [null]
+      description1: string | null
       description2: string | null
+      description3: string | null
     }[]
   >([
     {
       title: null,
-      description1: [null],
+      description1: null,
       description2: null,
+      description3: null,
     },
   ])
+  console.log(section)
+
+  const handleSectionTitleChange = (
+    index: number,
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    const newSection = [...section]
+    newSection[index].title = event.target.value
+    setSection(newSection)
+  }
+
+  const handleSectionDescription1Change = (
+    index: number,
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    const newSection = [...section]
+    newSection[index].description1 = event.target.value
+    setSection(newSection)
+  }
+
+  const handleSectionDescription2Change = (
+    index: number,
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    const newSection = [...section]
+    newSection[index].description2 = event.target.value
+    setSection(newSection)
+  }
+
+  const handleSectionDescription3Change = (
+    index: number,
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    const newSection = [...section]
+    newSection[index].description3 = event.target.value
+    setSection(newSection)
+  }
+
+  // Função para adicionar uma nova seção
+  const handleAddSection = () => {
+    setSection([
+      ...section,
+      {
+        title: null,
+        description1: null,
+        description2: null,
+        description3: null,
+      },
+    ])
+  }
 
   return (
     <div>
-      <div className="laptop:max-w-[600px] mx-auto flex max-w-[1000px] flex-col rounded-xl border bg-white p-2 text-black shadow-2xl ">
+      <div className="mx-auto flex max-w-[1000px] flex-col rounded-xl border bg-white p-2 text-black shadow-2xl laptop:max-w-[600px] ">
         <div className="flex justify-end">
           <button
             //   onClick={handlePushAddRoute}
-            className="mobile:text-xs laptop:text-base flex items-center justify-between gap-2 rounded bg-green-600 px-4 py-2 text-lg font-medium text-white hover:bg-opacity-95"
+            className="flex items-center justify-between gap-2 rounded bg-green-600 px-4 py-2 text-lg font-medium text-white hover:bg-opacity-95 mobile:text-xs laptop:text-base"
           >
             <Printer size={20} />
             <p>Imprimir</p>
@@ -31,40 +90,59 @@ export default function Design() {
         </div>
         <div about="Titulo" className="flex justify-center py-1">
           <input
-            className=" w-full border bg-transparent px-2 text-center"
+            className="w-full border bg-transparent px-2 text-center text-2xl font-medium"
             type="text"
+            value={title}
+            onChange={handleTitleChange} // Função onChange para o título principal
           />
         </div>
         <div className="flex flex-col gap-5">
-          <div className="border border-zinc-400">
-            <div className="bg-green-600">
-              <input className=" w-full bg-transparent px-2" type="text" />
-            </div>
-            <div className=" grid grid-cols-5 items-center bg-green-600  bg-opacity-25">
-              <div className="col-span-2  text-sm">
-                <input className=" w-full bg-transparent px-2" type="text" />
-              </div>
-              <div className="col-span-3 border-l border-zinc-400  text-sm">
-                <input className=" w-full bg-transparent px-2" type="text" />
-              </div>
-            </div>
-            <div className=" grid grid-cols-5 items-center">
-              <div className="col-span-2  text-sm">
-                <input className=" w-full bg-transparent px-2" type="text" />
-              </div>
-              <div className="col-span-3 border-l border-zinc-400  text-sm">
-                <input className=" w-full bg-transparent px-2" type="text" />
-              </div>
-            </div>
-            <div className=" grid grid-cols-5 items-center bg-green-600  bg-opacity-25">
-              <div className="col-span-5 text-sm">
+          {section.map((sec, index) => (
+            <div key={index} className="border border-zinc-400">
+              <div className="bg-green-600">
                 <input
-                  className=" w-full bg-transparent px-2 py-1"
+                  className="w-full bg-transparent px-2 text-xl font-medium"
                   type="text"
+                  value={sec.title || ''}
+                  onChange={(e) => handleSectionTitleChange(index, e)} // Função onChange para o título da seção
                 />
               </div>
+              <div className="grid grid-cols-5 items-center bg-green-600 bg-opacity-25">
+                <div className="col-span-2 text-sm">
+                  <input
+                    className="w-full bg-transparent px-2 text-lg font-medium"
+                    type="text"
+                    value={sec.description1 || ''}
+                    onChange={(e) => handleSectionDescription1Change(index, e)} // Função onChange para description1
+                  />
+                </div>
+                <div className="col-span-3 border-l border-zinc-400 text-sm">
+                  <input
+                    className="w-full bg-transparent px-2 text-lg font-medium"
+                    type="text"
+                    value={sec.description2 || ''}
+                    onChange={(e) => handleSectionDescription2Change(index, e)} // Função onChange para description2
+                  />
+                </div>
+              </div>
+              <div className="grid grid-cols-5 items-center bg-green-600 bg-opacity-25">
+                <div className="col-span-5 text-sm">
+                  <input
+                    className="w-full bg-transparent px-2 py-1 text-lg font-medium"
+                    type="text"
+                    value={sec.description3 || ''}
+                    onChange={(e) => handleSectionDescription3Change(index, e)} // Função onChange para description3
+                  />
+                </div>
+              </div>
             </div>
-          </div>
+          ))}
+          <button
+            onClick={handleAddSection} // Adicionando nova seção ao clicar
+            className="border-3 flex h-32 items-center justify-center border-4 border-dashed bg-green-400 bg-opacity-20"
+          >
+            <Plus size={70} className="text-zinc-400" />
+          </button>
         </div>
       </div>
     </div>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,18 +6,18 @@ import { FileDown, FilePlus } from 'lucide-react'
 export const Hero = () => {
   return (
     <>
-      <Container className="flex flex-wrap ">
-        <div className="flex w-full items-center lg:w-1/2">
+      <Container className="flex  ">
+        <div className="lg:w-1/2 flex w-full items-center">
           <div className="mb-8 max-w-2xl">
-            <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 dark:text-white lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight">
+            <h1 className="lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight text-4xl font-bold leading-snug tracking-tight text-gray-800 dark:text-white">
               Crie Prescrições de forma simplificada e personalizada
             </h1>
-            <p className="py-5 text-xl leading-normal text-gray-500 dark:text-gray-300 lg:text-xl xl:text-2xl">
+            <p className="lg:text-xl xl:text-2xl py-5 text-xl leading-normal text-gray-500 dark:text-gray-300">
               Gere prescrições manuais ou a partir de modelos pré-salvos por
               você
             </p>
 
-            <div className="flex flex-col items-start space-y-3 sm:flex-row sm:items-center sm:space-x-4 sm:space-y-0">
+            <div className="sm:flex-row sm:items-center sm:space-x-4 sm:space-y-0 flex items-center gap-5">
               <a
                 href="/templates"
                 className="flex items-center gap-2 rounded-md bg-indigo-600 px-8 py-4 text-center text-lg font-medium text-white "
@@ -35,7 +35,7 @@ export const Hero = () => {
             </div>
           </div>
         </div>
-        <div className="flex w-full items-center justify-center lg:w-1/2">
+        <div className="lg:w-1/2 flex w-full items-center justify-center">
           <div className="">
             <Image
               src={heroImg}
